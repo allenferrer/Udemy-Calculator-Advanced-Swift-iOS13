1f3a0c18e8fb1b2dba44ca1c5201f9602622c16c
Allen Ferrer <allen.ferrer@xurpas.com>
2021-01-27 10:33:19 +0800

    Add basic features - Add basic data entry - Fix of String to Double assignments

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,19 +12,28 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    
+    private var isFinishedTyping: Bool = true
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
-        
+        isFinishedTyping = true
         //What should happen when a non-number button is pressed
-    
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert text to double")
+        }
+        
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
-        
         //What should happen when a number is entered into the keypad
-    
+        if let numValue = sender.currentTitle {
+            if isFinishedTyping {
+                displayLabel.text = numValue
+                isFinishedTyping = false
+            } else {
+                displayLabel.text?.append(numValue)
+            }
+        }
     }
 
 }
